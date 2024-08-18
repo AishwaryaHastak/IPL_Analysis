8057ef5fcb38acd73ba9b33313160e136546f0c8
Aishwarya Hastak <h4hastak@gmail.com>
2024-08-18 15:17:29 -0400

    requirements updated

---
+++ constants.py
@@ -0,0 +1,5 @@
+
+# MATCHES_FILE_PATH = '/dbfs/FileStore/default/matches.csv'
+# DELIVERIES_FILE_PATH = '/dbfs/FileStore/default/deliveries.csv'
+MATCHES_FILE_PATH = 'matches.csv'
+DELIVERIES_FILE_PATH = 'deliveries.csv'
